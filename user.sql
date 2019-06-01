04b8baab030c564fd267173159f0cab76440a8aa
vegchic <h_foryou@163.com>
2019-06-01 13:50:20 +0800

    fix: 增加 avatar（头像文件名）

--- user.sql
+++ user.sql
@@ -1,10 +1,12 @@
-create table user(
-  uid int unsigned not null primary key auto_increment,
-  password varchar(40) not null, 
-  nick_name varchar(40) not null,
-  age smallint unsigned not null,
-  gender char(1) not null,
-  balance float default 0.0,
-  phone varchar(40) not null,
-  email varchar(40) not null unique
-)DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `swsad`.`user` (
+  `uid` INT NOT NULL AUTO_INCREMENT,
+  `nickname` VARCHAR(45) NOT NULL DEFAULT '小虾米',
+  `email` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(45) NULL,
+  `gender` VARCHAR(45) NOT NULL DEFAULT 'unknown',
+  `balance` INT NOT NULL DEFAULT 0,
+  `age` INT NOT NULL DEFAULT 18,
+  `avatar` VARCHAR(255) NOT NULL DEFAULT 'default',
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
