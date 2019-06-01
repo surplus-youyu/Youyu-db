43ada27dd36a894e3b4aa03242059eeda2ee1d67
vegchic <h_foryou@163.com>
2019-06-01 15:08:03 +0800

    fix：调整字段属性

--- user.sql
+++ user.sql
@@ -1,12 +1,13 @@
-CREATE TABLE `swsad`.`user` (
-  `uid` INT NOT NULL AUTO_INCREMENT,
-  `nickname` VARCHAR(45) NOT NULL DEFAULT '小虾米',
-  `email` VARCHAR(45) NOT NULL,
-  `phone` VARCHAR(45) NULL,
-  `gender` VARCHAR(45) NOT NULL DEFAULT 'unknown',
-  `balance` INT NOT NULL DEFAULT 0,
-  `age` INT NOT NULL DEFAULT 18,
-  `avatar` VARCHAR(255) NOT NULL DEFAULT 'default',
-  `password` VARCHAR(45) NOT NULL,
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(45) NOT NULL DEFAULT '小虾米',
+  `email` varchar(45) NOT NULL,
+  `phone` varchar(45) NOT NULL DEFAULT '',
+  `gender` varchar(45) NOT NULL DEFAULT 'unknown',
+  `balance` int(11) NOT NULL DEFAULT '0',
+  `age` int(11) NOT NULL DEFAULT '18',
+  `avatar` varchar(255) NOT NULL DEFAULT 'default',
+  `password` varchar(45) NOT NULL,
   PRIMARY KEY (`uid`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
