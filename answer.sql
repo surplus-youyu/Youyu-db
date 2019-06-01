f05873976ecb2d56b7df8dbb586600736723ca1f
vegchic <h_foryou@163.com>
2019-06-01 14:48:56 +0800

    fix: 调整任务结果结构

--- answer.sql
+++ answer.sql
@@ -1,8 +1,19 @@
-create table answer(
-  sid int unsigned not null,
-  interviewee_id int unsigned not null,
-  content varchar(5000) not null,
-  primary key(sid, interviewee_id),
-  foreign key(sid) references survey(id),
-  foreign key(interviewee_id) references user(uid)
-)DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `swsad`.`task_result` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tid` INT NOT NULL,
+  `uid` INT NOT NULL,
+  `result` TEXT NOT NULL,
+  `enclosure` VARCHAR(1024) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `tr_uer_fk_idx` (`uid` ASC),
+  INDEX `tr_task_idx` (`tid` ASC),
+  CONSTRAINT `tr_uer_fk`
+    FOREIGN KEY (`uid`)
+    REFERENCES `swsad`.`user` (`uid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `tr_task`
+    FOREIGN KEY (`tid`)
+    REFERENCES `swsad`.`task` (`tid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
