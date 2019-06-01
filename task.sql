fe062fc593aa443de948aff504209442766c37b2
vegchic <h_foryou@163.com>
2019-06-01 15:07:10 +0800

    fix: 调整字段属性

--- task.sql
+++ task.sql
@@ -1,16 +1,13 @@
-CREATE TABLE `swsad`.`task` (
-  `tid` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(45) NOT NULL,
-  `summary` TEXT NULL,
-  `type` VARCHAR(45) NOT NULL,
-  `bounty` INT UNSIGNED NOT NULL DEFAULT 0,
-  `extra` TEXT NOT NULL,
-  `owner` INT NOT NULL,
-  `enclosure` VARCHAR(128) NULL,
+CREATE TABLE `task` (
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `summary` text,
+  `type` varchar(45) NOT NULL,
+  `bounty` int(10) unsigned NOT NULL DEFAULT '0',
+  `extra` text,
+  `owner` int(11) NOT NULL,
+  `enclosure` varchar(1024) NOT NULL DEFAULT '',
   PRIMARY KEY (`tid`),
-  INDEX `owner_id_idx` (`owner` ASC),
-  CONSTRAINT `owner_id`
-    FOREIGN KEY (`owner`)
-    REFERENCES `swsad`.`user` (`uid`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  KEY `owner_id_idx` (`owner`),
+  CONSTRAINT `owner_id` FOREIGN KEY (`owner`) REFERENCES `user` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
