368321e34fe1ec5d4013da7c909cbbc4d7cedefc
vegchic <h_foryou@163.com>
2019-06-24 19:38:59 +0800

    Use mysqldump

--- task_result.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `task_result` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `result` text NOT NULL,
-  `enclosure` varchar(1024) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
